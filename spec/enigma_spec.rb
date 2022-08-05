3c584ed42c2e2a2f77fe51a1ef3d01383f8f3697
Astrid Hecht <silentaviary@gmail.com>
2022-08-05 12:47:52 -0600

    test: Write enigma #encrypt and #decrypt tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,3 +1,60 @@
 require_relative '../lib/enigma'
 
-Rspec
+RSpec.describe Enigma do
+  before(:each) do
+    @enigma = Enigma.new
+  end
+
+  describe '#encrypt' do
+    it 'can encrypt a message with a key and date' do
+      expect(@enigma.encrypt('hello world', '02715', '040895')).to eq 
+      {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
+    end
+
+    it "can encrypt a message with a key (uses today's date)" do
+      expect(@enigma.enigma.encrypt('hello world', '02715')).to eq 
+      {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
+    end
+
+    it "can encrypt a message (generates random key and uses today's date)" do
+      expect(enigma.encrypt('hello world')).to eq
+      {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
+    end
+  end
+
+  describe '#decrypt' do
+    it 'can decrypt a message with a key and date' do
+      expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq
+      {
+        encryption: 'hello world',
+        key: '02715',
+        date: '040895'
+      }
+    end
+
+    it "can decrypt a message with a key (uses today's date)" do
+      encrypted = enigma.encrypt('hello world', '02715')
+      expect(@enigma.decrypt(encrypted[:encryption], '02715')).to eq
+      {
+        encryption: 'hello world',
+        key: '02715',
+        date: '040895'
+      }
+    end
+  end
+
+  # describe '#crack' do
+  # end
+end
