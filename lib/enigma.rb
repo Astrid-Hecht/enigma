78f1456579116257a38ede2521b71b63e757064c
Astrid Hecht <silentaviary@gmail.com>
2022-08-05 23:14:20 -0600

    feat: #date_gen helper method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,16 +1,24 @@
+require 'date'
+
 class Enigma
-def encrypt(message, key = nil, date = nil)
-  
-end
+  def encrypt(message, key = nil, date = nil)
+   
+  end
 
-# def decrypt(message, key, date = nil)
-# end
+  # def decrypt(message, key, date = nil)
+  # end
 
-# def crack
-# end
+  # def crack
+  # end
 
-private
+  private
 
-def key_gen
-  Random.new.rand(99999).to_s.rjust(5, '0')
-end
+  def key_gen
+    Random.new.rand(99999).to_s.rjust(5, '0')
+  end
+
+  def date_gen
+    date = Time.new
+    date.strftime("%d%m%y")
+  end
+    
