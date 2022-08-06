491fa20faa61f63bba7988599539caff44f8debb
Astrid Hecht <silentaviary@gmail.com>
2022-08-06 15:42:49 -0600

    write encrypt runner and message.txt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,14 @@
-require_relative './lib/enigma'
+require_relative './enigma'
+
+puts "Enter name of source file:"
+input = 'message.txt' #gets.chomp
+
+puts "Enter name of output file:"
+output = 'encrypted.txt' #gets.chomp
+
+message = File.read("./data/#{input}")
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
+File.open("./data/#{output}", 'w') do |f|
+  f.write(encrypted[:encryption])
+end
