5a9d19eaddb33ab91412946d0808bb2909d8cb83
Astrid Hecht <silentaviary@gmail.com>
2022-08-05 23:02:55 -0600

    feat: #key_gen for when key is absent

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,16 @@
 class Enigma
+def encrypt(message, key = nil, date = nil)
+  
+end
 
+# def decrypt(message, key, date = nil)
+# end
+
+# def crack
+# end
+
+private
+
+def key_gen
+  Random.new.rand(99999).to_s.rjust(5, '0')
 end
